7962b58286e9b40f096ac2f608a7f0e95c21ad98
Anna Ö <anna.ostman24@gmail.com>
2024-01-31 19:36:47 +0200

    Create Angular project with Server side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FundComponent} from "./components/fund/fund.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'crosskey_code_test_angular';
+  title = "crosskey_code_test_angular"
 }
